26803c671c187b08de2358a4871b3a3d7d768f13
Skyler Littleaxe <littleaxe.skyler@gmail.com>
2020-03-24 01:28:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,13 +2,18 @@ class Song
   attr_accessor :name, :artist, :genre
   
   @@count = 0
+  @@genre = []
   
   def initialize(name, artist, genre)
     @name = name
     @artist = artist
     @genre = genre
     @@count += 1
+    @@genre 
   end
   
+  def self.count
+    @@count 
+  end
   
 end
